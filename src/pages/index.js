a4f61e960aecf7de3f47859583e00cae3d50ade9
Bryanacosta3205 <Bryanacosta3205@gmail.com>
2021-03-26 20:31:13 -0600

    new

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,41 @@
 import { Link } from "gatsby"
-import React from "react"
+import React, { useEffect, useRef, useState } from "react"
 
 export default function Home() {
+  const isMounted = useRef(true)
+  const [state, setstate] = useState()
+
+  useEffect(()=>{
+    return ()=>{
+      isMounted.current = false;
+    }
+  },[])
+
+  useEffect(() => {
+    getImage()
+  }, [state])
+
+  const getImage = async ()=>{
+    
+      try {
+        const {url} = await fetch("https://source.unsplash.com/random/350x200");
+        if (isMounted.current) {
+          setstate(url)
+        }
+      } catch (error) {
+        alert("An error has ocurred "+error.message)
+      }
+    
+  }
+
+
   return (
     <div style={{ color: `purple` }}>
       <Link to="/contact/">Contact</Link>
       <h1>Hello Gatsby!</h1>
       <p>What a world.</p>
-      <img src="https://source.unsplash.com/random/400x200" alt="" />
+      
+      {state?<img src={state} alt="" />:"Loading image..."}
     </div>
   )
 }
